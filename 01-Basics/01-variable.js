982fbcd76ab643090b8891eed1538ae9fab51492
Suniil Ghate <sunil.82ghate@gmail.com>
2025-04-16 22:19:31 +0530

    New Files

---
+++ 01-Basics/01-variable.js
@@ -0,0 +1,11 @@
+const accountId = 123456
+let accountEmail = "test@testdom.com"
+var accountPassword = "111111"
+accountCity = "Nashik"
+let accountState;
+
+//accountId = 3444 //Not allowed
+accountEmail = "sunil.ghate@gmail.com"
+accountCity = 'Pune'
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
